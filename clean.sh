0533957da1b42179f89b9b28bebcc7e15cd5994b
Kamron <ebrahimikam@gmail.com>
2020-05-22 15:08:18 -0700

    network setup using static routing in busybox container

--- clean.sh
+++ clean.sh
@@ -2,10 +2,20 @@
 
 docker-compose rm -f
 
+docker kill brozeekegressfilter_attacker_router_1 \
+			brozeekegressfilter_victim_router_1 \
+			brozeekegressfilter_victim_1 \
+			brozeekegressfilter_attacker_1 \ 
+
+docker container rm $(docker ps -a -q)
+
 docker image rm brozeekegressfilter_attacker \
 		brozeekegressfilter_victim \
+		brozeekegressfilter_reflection \
 		brozeekegressfilter_attacker_router  
 
 docker network rm \
 	brozeekegressfilter_attacker_net \
-	brozeekegressfilter_victim_net	 
+	brozeekegressfilter_victim_net \
+	brozeekegressfilter_reflection_net \
+	brozeekegressfilter_router_net
